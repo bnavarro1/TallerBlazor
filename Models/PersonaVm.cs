da219c329101f9941e1d075dcd828eca07716ad8
bnavarro <bnavarro@TO-PCW10BNAVARR>
2023-07-26 09:44:21 -0400

    Agregar archivos de proyecto.

---
+++ Models/PersonaVm.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerBlazor.Models
+{
+	public class PersonaVm
+	{
+		[Required(ErrorMessage = "No puede estar en Blanco.")]
+		[RegularExpression(@"[0-9]{8}[-]{1}[0-9Kk]{1}",ErrorMessage ="Debe ingresar formato del Rut.")]
+		public string? Rut{ get; set; }
+
+		[Required(ErrorMessage = "No puede estar en Blanco.")]
+		[MaxLength(60, ErrorMessage ="Debe ingresar máximo 60 caracteres.")]
+		public string? Nombre { get; set; }
+
+		[Required(ErrorMessage = "No puede estar en Blanco.")]
+		public DateTime FechaNac{ get; set; }
+
+		[Required(ErrorMessage = "No puede estar en Blanco.")]
+		[MaxLength(10, ErrorMessage = "Debe ingresar máximo 10 caracteres.")]
+		public string? User { get; set; }
+
+		[Required(ErrorMessage = "No puede estar en Blanco.")]
+		[MaxLength(10, ErrorMessage = "Debe ingresar máximo 10 caracteres.")]
+		[RegularExpression(@"[A-Z]{1}[A-Za-z0-9]{9}", ErrorMessage = "Debe ingresar la primera letra mayúsculo.")]
+		public string? Password { get; set; }
+
+
+	}
+}
---
+++ Pages/Sesion02.razo.cs
@@ -0,0 +1,28 @@
+﻿using TallerBlazor.Models;
+
+namespace TallerBlazor.Pages
+{
+	public partial class Sesion02
+	{
+		private PersonaVm ModelPersona { get; set; } = new();
+		private string MiTitulo { get; set; } = "Mantenedor de Usuarios";
+
+		private void ok()
+		{
+
+		}
+
+
+		private void Aceptar()
+		{
+			Console.WriteLine("OK....");
+		}
+
+		private void Cancelar()
+		{
+			ModelPersona = new();
+		}
+
+	}
+
+}
